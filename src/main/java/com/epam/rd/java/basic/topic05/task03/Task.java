3330c05edddc687ea751f51e13bb36ce96a96be9
Vitalii Kalinchyk <karlkory@gmail.com>
2022-09-23 17:56:58 +0300

    Better one

--- src/main/java/com/epam/rd/java/basic/topic05/task03/Task.java
+++ src/main/java/com/epam/rd/java/basic/topic05/task03/Task.java
@@ -21,65 +21,52 @@ public class Task {
 	public void compare() {
 		c1 = 0;
 		c2 = 0;
+		Thread[] threads = new Thread[numberOfThreads];
 		for (int i = 0; i < numberOfThreads; i++) {
-			Thread thread = new Thread(this::counting);
-			thread.start();
+			threads[i] = new Thread(this::counting);
+			threads[i].start();
 		}
-		try {
-			Thread.sleep(150);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException ignored) {}
 		}
 	}
 
 	public void compareSync() {
 		c1 = 0;
 		c2 = 0;
+		Thread[] threads = new Thread[numberOfThreads];
 		for (int i = 0; i < numberOfThreads; i++) {
-			Thread thread = new Thread(this::countingSync);
-			thread.start();
+			threads[i] = new Thread(() -> {
+				synchronized (this) {
+					counting();
+				}
+			});
+			threads[i].start();
 		}
-		try {
-			Thread.sleep(150);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException ignored) {}
 		}
 	 }
 
 	private void counting() {
-		c1 = 0;
-		c2 = 0;
 		for (int j = 0; j < numberOfIterations; j++) {
 			c1++;
 			try {
 				Thread.sleep(pause);
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
+			} catch (InterruptedException ignored) {}
 			c2++;
 			System.out.println((c1 == c2) + " " + c1 + " " + c2);
 		}
 	}
-	synchronized private void countingSync() {
-		c1 = 0;
-		c2 = 0;
-		for (int j = 0; j < numberOfIterations; j++) {
-			c1++;
-			try {
-				Thread.sleep(pause);
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
-			c2++;
-			System.out.println((c1 == c2) + " " + c1 + " " + c2);
-		}
-	}
-	
+
 	public static void main(String[] args) {
 		Task t = new Task(2, 5, 10);
 		t.compare();
 		System.out.println("~~~");
 		t.compareSync();
 	}
-
 }
